bd3963cc9a57faabc65c62bd4bedca77869225dc
Songminseon <smsun0329@naver.com>
2022-06-18 00:05:57 +0900

    유저정보 추가

--- router/index.js
+++ router/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const homeRouter = require("./home");
+const userRouter = require("./user");
 
 router.use("/home", homeRouter);
+router.use("/user", userRouter);
 
 module.exports = router;
---
+++ router/user.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const result = {
+    nickname: "minseon",
+    imgUrl: "https://gdsc-seoultech.github.io/img/square_logo.png",
+  };
+  return res.status(200).send(result);
+});
